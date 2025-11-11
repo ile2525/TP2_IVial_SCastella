cce56e6fb89a62071464cdf9145430974d15df3c
ileona <2405565@bdeb.qc.ca>
2025-11-11 18:30:24 -0500

    html/css des tendances - librairie foutu -

--- src/librairie1.js
+++ src/librairie1.js
@@ -1,8 +1,7 @@
 import * as THREE from 'three/webgpu';
 
 			import Stats from 'three/examples/jsm/libs/stats.module.js';
-
-			import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+			
 
 
           
--- src/main.js
+++ src/main.js
@@ -4,11 +4,10 @@ import * as THREE from 'three/webgpu';
 import { color, cos, float, mix, range, sin, time, uniform, uv, vec3, vec4, PI2 } from 'three/tsl';
 
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Création des paramètres couleurInterne et couleurExterne pour rendre les instances modifiables
-function duplicate(couleurInterne = '#ffa575', couleurExterne = '#311599') {
+function duplicate() {
 	let camera, scene, renderer, controls;
 
 	init();
@@ -53,8 +52,8 @@ function duplicate(couleurInterne = '#ffa575', couleurExterne = '#311599') {
 		material.positionNode = position.add(randomOffset);
 
 		// Ajout des paramètre couleurInterne et couleurExterne
-		const colorInside = uniform(color(couleurInterne));
-		const colorOutside = uniform(color(couleurExterne));
+		const colorInside = uniform(color('#ffa575'));
+		const colorOutside = uniform(color('#311599'));
 		const colorFinal = mix(colorInside, colorOutside, radiusRatio.oneMinus().pow(2).oneMinus());
 		const alpha = float(0.1).div(uv().sub(0.5).length()).sub(0.2);
 		material.colorNode = vec4(colorFinal, alpha);
@@ -63,6 +62,7 @@ function duplicate(couleurInterne = '#ffa575', couleurExterne = '#311599') {
 		scene.add(mesh);
 
 		// renderer
+
 
 		renderer = new THREE.WebGPURenderer({ antialias: true });
 		renderer.setPixelRatio(window.devicePixelRatio);
@@ -73,7 +73,7 @@ function duplicate(couleurInterne = '#ffa575', couleurExterne = '#311599') {
 		controls = new OrbitControls(camera, renderer.domElement);
 
 		// Desactive les controles de la camera 
-		controls.enableDamping = false;
+		controls.enableDamping = true;
 		controls.enabled = false;
 
 		controls.minDistance = 0.1;
@@ -104,7 +104,7 @@ function duplicate(couleurInterne = '#ffa575', couleurExterne = '#311599') {
 	renderer.setSize(1000, 600)
 }
 
-// duplicate('galaxy1', '#e47b42ff', '#991515ff');
-// duplicate('galaxy2', '#2a8612ff', '#74580cff');
+
+
 
 
