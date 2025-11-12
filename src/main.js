76f6d84bc59489c175b6755396180ecca03379ca
ile2525 <2405565@bdeb.qc.ca>
2025-11-12 13:12:57 -0500

    Modification js pour carrousel

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import './style.css'
 
-
-const swiper = new Swiper(".mySwiper", {
-	slidesPerView: 3,
-	spaceBetween: 50,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+// Ajout des document.querySelector pour rendre le carrousel applicable plusieurs fois
+document.querySelectorAll('.mySwiper').forEach((element) => {
+	new Swiper(element, {
+		slidesPerView: 3,
+		spaceBetween: 50,
+		navigation: {
+			nextEl: element.querySelector('.swiper-button-next'),
+			prevEl: element.querySelector('.swiper-button-prev'),
+		},
+	});
 });
+
 
 
 
