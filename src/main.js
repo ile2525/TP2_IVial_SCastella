1d4f892be6ea62e5380c13c7cabf7d84cae43aec
ileona <2405565@bdeb.qc.ca>
2025-11-12 00:16:45 -0500

    base de carrousel

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import './style.css'
 
 
+const swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	spaceBetween: 50,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+
 
 
 
