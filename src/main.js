bc62de736150b07e0b0e2a9b46c7d30b16370050
ChoupiPlanete <1064152@bdeb.qc.ca>
2025-11-13 00:29:44 -0500

    breakpoint js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,32 @@ import './style.css'
 
 // Ajout des document.querySelector pour rendre le carrousel applicable plusieurs fois----------Iléona
 document.querySelectorAll('.mySwiper').forEach((element) => {
-	new Swiper (element, {
-		slidesPerView: 3,
-		spaceBetween: 50,
-		navigation: {
-			nextEl: element.querySelector('.swiper-button-next'),
-			prevEl: element.querySelector('.swiper-button-prev'),
-		},
-	});
+  new Swiper(element, {
+    slidesPerView: 3, 
+    spaceBetween: 50,
+    navigation: {
+      nextEl: element.querySelector('.swiper-button-next'),
+      prevEl: element.querySelector('.swiper-button-prev'),
+    },
+    breakpoints: {
+	  0: {          
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      610: {        
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {        
+        slidesPerView: 3,
+        spaceBetween: 40,
+      }
+    }
+  });
 });
+
+//Sandra: comme on a pas vu le responsive pour les librairies, on a utilisé cette source et chatgpt m'a aidé à l'intégrer les breakpoints
+//https://swiperjs.com/types/interfaces/types_swiper_options.SwiperOptions#breakpoints
 
 
 //----------affichage du menu burger--------------------------Sandra
